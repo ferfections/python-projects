f608c5ddbc038ddcef44c2885e6eb0e441c03dea
ferfections <fng8@alu.ua.es>
2025-10-12 11:29:36 +0200

    Update main.py

--- cryptography/main.py
+++ cryptography/main.py
@@ -1,15 +1,26 @@
 from cryptography.fernet import Fernet
 
+def encriptar(mensaje: bytes, llave):
+    f = Fernet(llave)
+    return f.encrypt(mensaje)
+
+def desencriptar(token: bytes, llave):
+    f = Fernet(llave)
+    return f.decrypt(token)
+
+
 def main():
     print("Módulo de criptografía")
 
     mensaje = "Este es el mensaje super secreto"
+    llave: bytes = Fernet.generate_key()
+    token = encriptar(bytes(mensaje, 'utf-8'), llave)
+    desencriptado = desencriptar(token, llave)
+    print(desencriptado)
 
-    key = Fernet.generate_key()
-    f = Fernet(key)
-    token = f.encrypt(bytes(mensaje, 'utf-8'))
 
-    print(f.decrypt(token))
+
+
     
 
     
